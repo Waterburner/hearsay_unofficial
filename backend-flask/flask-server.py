494b625096b6bbd328194931700a6a76a0049768
Waterburner <Waterburner@users.noreply.github.com>
2022-04-03 21:30:41 -0500

    configured db

--- backend-flask/flask-server.py
+++ backend-flask/flask-server.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
+
+#configure db 
+db = yaml.safe_load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
 
 @app.route("/", methods=['GET', 'POST'])
 def home():

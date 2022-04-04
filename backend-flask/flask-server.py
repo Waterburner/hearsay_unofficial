0369f49df26a43196e86933dbd3e124006d02cf5
Waterburner <Waterburner@users.noreply.github.com>
2022-04-04 17:48:05 -0500

    setting up and testing backend-flask

--- backend-flask/flask-server.py
+++ backend-flask/flask-server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+
+from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 import yaml
 
@@ -13,14 +14,42 @@ app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
 
-@app.route("/", methods=['GET', 'POST'])
-def home():
-    return #something
+# @app.route("/", methods=['GET', 'POST'])
+# def home():
+    # return #something
 
-@app.route("/allmenus")
-def allMenus():
+# @app.route("/allmenus")
+# def allMenus():
 
-    if request.method == 'GET':
+# =================== testing ==================
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        name = userDetails['name']
+        email = userDetails['email']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)",(name, email))
+        mysql.connection.commit()
+        cur.close()
+        print('success')
+        return redirect('/users')
+    return render_template('index.html')
+
+@app.route("/test")
+def test():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM menus")
+    if resultValue > 0:
+        userDetails = cur.fetchall()
+        return render_template('users.html', userDetails=userDetails)
+
+# =================== end of testing ==================
+        
+
+    # if request.method == 'GET':
         #fetch data
         
         # all_menus_id = [ SELECT menus_id FROM menus; ] in array
@@ -29,11 +58,11 @@ def allMenus():
         #or
         # all_menus_data = [menus_id[0], menus_name[0], menus_id[1], menus_name[1] … menus_id[n], menus_name[n] ]
 
-        cur = mysql.connection.cursor()
-        resultValue = cur.execute("SELECT * from menus")
-        if resultValue > 0:
-            all_menus_data = cur.fetchall()
-            return #something?
+        # cur = mysql.connection.cursor()
+        # resultValue = cur.execute("SELECT * from menus")
+        # if resultValue > 0:
+        #     all_menus_data = cur.fetchall()
+        #     return #something?
 
 if __name__ == "__main__":
     app.run(debug=True)

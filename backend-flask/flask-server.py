704acc606a4fd72178d43657254034601463dcc4
Waterburner <Waterburner@users.noreply.github.com>
2022-04-03 21:56:15 -0500

    creating plan for flask-server

--- backend-flask/flask-server.py
+++ backend-flask/flask-server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_mysqldb import MySQL
 import yaml
 
@@ -8,23 +8,32 @@ app = Flask(__name__)
 db = yaml.safe_load(open('db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_PASSWORD'] = db['mysql_password'] 
 app.config['MYSQL_DB'] = db['mysql_db']
 
+mysql = MySQL(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return "Hello, I'm flask!"
+    return #something
 
-# Members API Route
+@app.route("/allmenus")
+def allMenus():
 
-# 
+    if request.method == 'GET':
+        #fetch data
+        
+        # all_menus_id = [ SELECT menus_id FROM menus; ] in array
+        # all_menus_name = [ SELECT menus_name FROM menus; ] in array
 
-@app.route("/members")
-def members():
-    return {"members": ["member1", "Member2", "Member3"]}
+        #or
+        # all_menus_data = [menus_id[0], menus_name[0], menus_id[1], menus_name[1] … menus_id[n], menus_name[n] ]
 
-
+        cur = mysql.connection.cursor()
+        resultValue = cur.execute("SELECT * from menus")
+        if resultValue > 0:
+            all_menus_data = cur.fetchall()
+            return #something?
 
 if __name__ == "__main__":
     app.run(debug=True)

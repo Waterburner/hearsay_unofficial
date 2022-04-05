4f2b6b151c22cbfd9c1960d500250d82128ee3d8
Waterburner <Waterburner@users.noreply.github.com>
2022-04-04 21:10:57 -0500

    Preset 'get all menus' for backend, and 'get certain menu'

--- backend-flask/flask-server.py
+++ backend-flask/flask-server.py
@@ -14,21 +14,41 @@ app.config['MYSQL_DB'] = db['mysql_db']
 
 mysql = MySQL(app)
 
-# @app.route("/", methods=['GET', 'POST'])
-# def home():
-    # return #something
+@app.route("/")
+def home():
+    greeting = "<h1>Hearsay API </h1>"
+    return greeting
 
-# @app.route("/allmenus")
-# def allMenus():
+#get all menus
+@app.route("/allmenus")
+def allMenus():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM menus")
+    menusDetails = cur.fetchall()
+    return render_template('index.html', menusDetails=menusDetails)
+    # returns: 
+    # menusDetails[0] - id
+    # menusDetails[1] - name of the menu
+
+
+# get certain menu
+@app.route(f"/menu_id=<id>")
+def certainMenu(id):
+    cur = mysql.connection.cursor()
+    # cur.execute(f"SET @menu_id = {id}")
+    cur.execute(f"SELECT * FROM menus WHERE menus_id={id}") # menu_id is variable
+    menusDetails = cur.fetchall()
+    return render_template('index.html', menusDetails=menusDetails) 
 
 # =================== testing ==================
 
-class AllMenus:
-    def __init__(menu, number, id, name):
-        menu.id = id
-        menu.name = name
+# class AllMenus:
+#     def __init__(menu, number, id, name):
+#         menu.id = id
+#         menu.name = name
 
-@app.route('/', methods=['GET'])
+# @app.route('/', methods=['GET'])
+
 # def index():
     # Fetch form data
     # cur = mysql.connection.cursor()
@@ -36,42 +56,28 @@ class AllMenus:
     # menusDetails = cur.fetchall()
     # return render_template('index.html', menusDetails=menusDetails)
 
-def index():
-    # Fetch form data
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM menus")
-    menusDetails = cur.fetchall()
+# def index():
+#     # Fetch form data
+#     cur = mysql.connection.cursor()
+#     cur.execute("SELECT * FROM menus")
+#     menusDetails = cur.fetchall()
     
-    m1 = menusDetails
+#     m1 = menusDetails
 
-    return render_template('index.html', menusDetails=menusDetails, m1=m1)
+#     return render_template('index.html', menusDetails=menusDetails, m1=m1)
 
 
-@app.route("/test")
-def test():
-    cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM menus")
-    if resultValue > 0:
-        userDetails = cur.fetchall()
-        return render_template('users.html', userDetails=userDetails)
+# @app.route("/test")
+# def test():
+#     cur = mysql.connection.cursor()
+#     resultValue = cur.execute("SELECT * FROM menus")
+#     if resultValue > 0:
+#         userDetails = cur.fetchall()
+#         return render_template('users.html', userDetails=userDetails)
 
 # =================== end of testing ==================
         
 
-    # if request.method == 'GET':
-        #fetch data
-        
-        # all_menus_id = [ SELECT menus_id FROM menus; ] in array
-        # all_menus_name = [ SELECT menus_name FROM menus; ] in array
-
-        #or
-        # all_menus_data = [menus_id[0], menus_name[0], menus_id[1], menus_name[1] … menus_id[n], menus_name[n] ]
-
-        # cur = mysql.connection.cursor()
-        # resultValue = cur.execute("SELECT * from menus")
-        # if resultValue > 0:
-        #     all_menus_data = cur.fetchall()
-        #     return #something?
 
 if __name__ == "__main__":
     app.run(debug=True)

20c275a6798505adeac7d5bcea44e1e458e6f1f9
Waterburner <Waterburner@users.noreply.github.com>
2022-05-11 00:26:58 -0500

    now allmenus being fetched from backend api (mysql db)

--- src/components/menu/Menus.js
+++ src/components/menu/Menus.js
@@ -45,6 +45,7 @@
 // }
 
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class Menus extends Component {
     constructor() {
@@ -55,8 +56,7 @@ export default class Menus extends Component {
             menus: [],
             isLoading: true,
         };
-
-        this.getMenus = this.getMenus.bind(this);
+        this.getMenus();
     }
 
     backend_server = "http://localhost:5000/allmenus";
@@ -64,11 +64,41 @@ export default class Menus extends Component {
     getMenus() {
         fetch(this.backend_server)
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.setState({ data: data }));
+        // response
+        // 0 [1, "Dinner menu", "dinner_menu", "https://drive.google.com/drive/u/1/folders/1PPo7mxtq50rgDKTVTg3gtPWYFHzyPr4y", "https://imgbox.com/g/MB5BuqDEI2", null] (6)
+        // 1 [2, "Brunch menu", "brunch_menu", null, null, null] (6)
+        // 2 [3, "Drink menu", "drink_menu", null, null, null] (6)
+        // 3 [4, "Dessert menu", "dessert_menu", null, null, null] (6)
+        // 4 [5, "Appetizers menu", "appetizers_menu", null, null, null] (6)
+    }
+
+    listMenus() {
+        return this.state.data.map((data) => {
+            return (
+                <li>
+                    <NavLink to={data[2]}>{data[1]}</NavLink>
+                </li>
+            );
+        });
     }
 
     render() {
-        this.getMenus();
-        return <div>getMenus</div>;
+        return (
+            <div className="menus-wrapper">
+                <div className="menus">
+                    <h2 className="menus-head-wrapper">
+                        <div className="menus-head">Menus</div>
+                    </h2>
+
+                    <div className="menu-selection-wrapper">
+                        <ul className="menu-selection">
+                            {this.listMenus()}
+                            <li>all dishes</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }

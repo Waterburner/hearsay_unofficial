5fae6bffe25a09e1b6ff87c6f2afe81a8c7d8f0a
Waterburner <Waterburner@users.noreply.github.com>
2022-05-11 22:51:23 -0500

    Created AllMenuItems and added import and Route

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Homepage from "./homepage/Homepage";
 import Menus from "./menu/Menus.js";
+import AllMenuItems from "./menu/AllMenuItems";
 import Dinner from "./menu/Dinner";
 import NoMatch from "./NoMatch";
 
@@ -25,6 +26,11 @@ export default class App extends Component {
                             <Route exact path="/" component={Homepage} />
 
                             <Route path="/menus" component={Menus} />
+
+                            <Route
+                                path="/allmenuitems"
+                                component={AllMenuItems}
+                            />
 
                             <Route path="/dinner_menu" component={Dinner} />
 
---
+++ src/components/menu/AllMenuItems.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class AllMenuItems extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            isLoading: false,
+            data: [],
+        };
+    }
+
+    render() {
+        return <div></div>;
+    }
+}

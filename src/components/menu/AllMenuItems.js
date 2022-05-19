c7478e1e001895369a2531968b685f069126d707
Waterburner <Waterburner@users.noreply.github.com>
2022-05-18 23:31:36 -0500

    data[2] saves as array of menu_name, but wont save as array without the index

--- src/components/menu/AllMenuItems.js
+++ src/components/menu/AllMenuItems.js
@@ -20,133 +20,22 @@ export default class AllMenuItems extends Component {
     getItems() {
         fetch(this.backend_server)
             .then((response) => response.json())
-            .then((data) => this.setState({ data: data }))
+            .then((data) => this.setState({ data: data[2] }))
+            .then(console.log(this.state.data))
             .catch((error) => {
                 console.log("getItems() fetch error", error);
             });
-        // response
-        // template: [0- menus_name, 1- menu-items, 1-0 - items from menus 0-0, 1-1 from 0-0 … 1-3 from 0-3…]
-        // [
-        //     [
-        //         ["dinner menu"],
-        //         ["brunch menu"],
-        //         ["drink menu"],
-        //         ["dessert menu"],
-        //         ["appetizers menu"],
-        //     ],
-        //     [
-        //         [
-        //             [
-        //                 1,
-        //                 "Crab crusted redfish",
-        //                 "Pan seared redfish, topped with crab crust and then backed to perfection. Served on the bed of garlic green beans with red pepper sauce and drizzled with lemon butter",
-        //                 "https://lh4.googleusercontent.com/TPg9FGHlToAD-JsFkUxQvvPxSwZ6hcEwhjFvq7isgix1fUeVqVKJoxnpu0W3BWqIGQdy7CKVGVkoIj3XsOuV=w1264-h856",
-        //                 null,
-        //                 1,
-        //             ],
-        //              …
-        //             [
-        //                 16,
-        //                 "Shrimp and Sausage Fettuccini",
-        //                 "saut\u00e9ed shrimp, jalape\u00f1o sausage, cherry tomato, Creole Sauce",
-        //                 "dinner menu link10",
-        //                 null,
-        //                 1,
-        //             ],
-        //         ],
-        //         [
-        //             [
-        //                 1,
-        //                 "Egg benedict",
-        //                 "Poached eggs served on the bed of muffin and proscuto, topped with hollanday sauce.",
-        //                 "https://test.com/sldkfj",
-        //                 null,
-        //                 2,
-        //             ],
-        //             [
-        //                 2,
-        //                 "Texas Omlette",
-        //                 "Omlette with smoked brisket, tomatoes, topped with avocado, pico, jallapenio",
-        //                 "https://testing.com/lk",
-        //                 null,
-        //                 2,
-        //             ],
-        //         ],
-        //         [
-        //             [
-        //                 1,
-        //                 "Whitney",
-        //                 "Respberry vodka, blackberry liquor cassis, muddle blackberry and champagne",
-        //                 "https://blah.com",
-        //                 null,
-        //                 3,
-        //             ],
-        //             [
-        //                 2,
-        //                 "TestItem3",
-        //                 "Destined for deletion as well",
-        //                 "no-link",
-        //                 null,
-        //                 3,
-        //             ],
-        //         ],
-        //         [
-        //             [
-        //                 1,
-        //                 "Bread pudding",
-        //                 "Bread pudding topped with caramel and acompanied with vanilla icecream",
-        //                 "https://test.com/sdkfsdlkfjsdlfkj",
-        //                 null,
-        //                 4,
-        //             ],
-        //             [
-        //                 2,
-        //                 "TestItem4?",
-        //                 "Another item destined for deletion as well",
-        //                 "no-link",
-        //                 null,
-        //                 4,
-        //             ],
-        //         ],
-        //         [],
-        //     ],
-        // ];
+        // # response{
+        // #   [0][menusID],
+        // #   [1][menusID][itemsID],
+        // #   [2][menu_name]
+        // # }
     }
 
     listItems() {
         console.log(this.state.data);
-        return this.state.data.map((data) => {
-            return (
-                <div>
-                    <div className="choosen-menu-wrapper">
-                        <div className="choosen-menu">
-                            <div className="choosen-menu-title-wrapper">
-                                <h2 className="choosen-menu-title">
-                                    {/* {data} */}
-                                    MENU-TEST
-                                </h2>
-                            </div>
-                        </div>
-                        {/* ====================================== call component "MenuItem" ========================================== */}
-
-                        <div className="menu-item-wrapper">
-                            <div className="menu-item">
-                                <div>
-                                    <div className="menu-item-title-wrapper">
-                                        <h3 className="menu-item-title"></h3>
-                                    </div>
-                                    <div className="menu-item-description-wrapper">
-                                        <div className="menu-item-description">
-                                            Pan seared redfish …
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* ====================================== END OF call component "MenuItem" ========================================== */}
-                    </div>
-                </div>
-            );
+        return this.state.data.map((item) => {
+            return <p>{item}</p>;
         });
     }
 

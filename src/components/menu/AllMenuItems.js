c7bf203b949036cdc176c9edf1c68c3403b4cb2d
Waterburner <Waterburner@users.noreply.github.com>
2022-06-02 17:16:57 -0500

    no actual changes

--- src/components/menu/AllMenuItems.js
+++ src/components/menu/AllMenuItems.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import MenuItem from "./MenuItem";
 // import MenuItem from "./MenuItem";
 
 export default class AllMenuItems extends Component {
@@ -7,24 +8,48 @@ export default class AllMenuItems extends Component {
 
         this.state = {
             isLoading: false,
-            data: [],
-            menus: [],
+            // item_menus: [],
+            // item_names: [],
+            // item_descriptions: [],
+            data: {
+                item_names: [],
+                item_descriptions: [],
+                item_img_links: [],
+                item_scan_links: [],
+                response: [],
+            },
         };
         this.getItems();
+        this.listItems();
     }
 
     // address to backend server (fetch data from DB)
     // backend_server_items = "http://localhost:5000/allitems";
     // backend_server_menus = "http://localhost:5000/allmenus";
+    // backend_server_item_names = "http://localhost:5000/allitems_detailed";
     backend_server = "http://localhost:5000/allitems_detailed";
 
     getItems() {
         fetch(this.backend_server)
             .then((response) => response.json())
-            .then((data) => this.setState({ data: data }))
+            .then((data) => this.setState({ data: { response: data } }))
             .catch((error) => {
                 console.log("AllMenuItems getItems() fetch error", error);
             });
+
+        //     fetch(this.backend_server_item_names)
+        //         .then((response) => response.json())
+        //         .then((data) => this.setState({ item_names: data }))
+        //         .catch((error) => {
+        //             console.log("AllMenuItems getItems() names fetch error", error);
+        //         });
+
+        //     fetch(this.backend_server_item_names)
+        //         .then((response) => response.json())
+        //         .then((data) => this.setState({ item_names: data }))
+        //         .catch((error) => {
+        //             console.log("AllMenuItems getItems() descriptions fetch error", error);
+        //         });
         // fetch(this.backend_server_items)
         //     .then((response) => response.json())
         //     .then((data) => this.setState({ data }))
@@ -44,29 +69,42 @@ export default class AllMenuItems extends Component {
     }
 
     listItems() {
+        this.setState({
+            data: { item_names: this.state.data.response },
+        });
+
         const data = this.state.data;
         let item_names = [];
-        item_names.push(this.state.data[0]);
+        item_names.push(this.state.data.response);
         let item_description = this.state.data[1];
         let item_img_link = this.state.data[2];
         let item_scan_link = this.state.data[3];
         let item_quantity = 0;
-
-        return item_names.map((item, count) => {
-            return (
-                <div>
-                    <h3>{count}</h3>
-                    <p>{item}</p>
-                </div>
-            );
-        });
+        console.log(this.state.data.item_names);
+        // return <p>{this.state.data.item_names}</p>;
+        // return item_names.map((item, count) => {
+        //     return (
+        //         <div>
+        //             <h3>{count}</h3>
+        //             <p>{item}</p>
+        //         </div>
+        //     );
+        // });
     }
 
     render() {
         // if (this.state.isLoading) {
         //     return <h3 className="loading">Loading…</h3>;
         // }
-
-        return <div>{this.listItems()}</div>;
+        return (
+            <div>
+                <MenuItem
+                    item_name="test"
+                    item_description="haha"
+                    item_img_link=""
+                    item_scan_link=""
+                />
+            </div>
+        );
     }
 }

d3709c727970d6aad81eb9d209c0c602c814d033
Waterburner <Waterburner@users.noreply.github.com>
2022-06-20 19:15:48 -0500

    added basic credits

---
+++ src/components/credits.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function () {
+    return (
+        <div className="credits-wrapper">
+            <div className="credits">
+                <h2>UNOFFICIAL TRAINING GUIDE</h2>
+                <p>
+                    Created by Oleh Kovelskyi as "best practices" and
+                    recommendations.
+                    <br />
+                    <br />
+                    <br />
+                    All rights reserved.
+                </p>
+            </div>
+        </div>
+    );
+}
